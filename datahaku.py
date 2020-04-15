bc6a2183fe1738910491ef7cce482e11fadc94e9
hiltar <tartsihiltsu@gmail.com>
2020-04-15 20:39:16 +0300

    ryönää

--- datahaku.py
+++ datahaku.py
@@ -1,7 +1,9 @@
 import csv
+import matplotlib.pyplot as plt
+import sklearn
 
 with open('data/vauvanimet.csv') as f:
     reader = csv.reader(f) ## Avaa readerin
     for row in reader:
         print(row)
-        
+## 
